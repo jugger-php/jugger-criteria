8c17f7605da2539bf3827a3e1610cd59f6fa7f2e
irpsv <i.rpsv@live.com>
2017-07-31 21:38:44 +0500

    change namespace

---
+++ src/criteria/MysqlCriteriaBuilder.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace jugger\query\criteria;
+
+class MysqlCriteriaBuilder extends CriteriaBuilder
+{
+    public function build(Criteria $criteria): string
+    {
+        $criteriaClass = get_class($criteria);
+        switch ($criteriaClass) {
+            case BetweenCriteria::class:
+                return $this->buildBetween($criteria);
+
+            case CompareCriteria::class:
+                return $this->buildCompare($criteria);
+
+            case EqualCriteria::class:
+                return $this->buildEqual($criteria);
+
+            case LikeCriteria::class:
+                return $this->buildLike($criteria);
+
+            case LogicCriteria::class:
+                return $this->buildLogic($criteria);
+
+            case RegexpCriteria::class:
+                return $this->buildRegexp($criteria);
+
+            default:
+                throw new \Exception("Not found class of criteria as '{$criteriaClass}'");
+        }
+    }
+
+    public function buildColumn(string $name)
+    {
+        return "`{$name}`";
+    }
+
+    public function buildLogic(LogicCriteria $criteria)
+    {
+        $operands = [];
+        $operator = strtoupper($criteria->getOperator());
+        foreach ($criteria->getValue() as $item) {
+            $sql = (new MysqlCriteriaBuilder)->build($item);
+            $operands[] = "({$sql})";
+        }
+        return join($operands, " {$operator} ");
+    }
+
+    public function buildBetween(BetweenCriteria $criteria)
+    {
+        $column = $this->buildColumn(
+            $criteria->getColumn()
+        );
+        $min = (float) $criteria->getMin();
+        $max = (float) $criteria->getMax();
+
+        return "{$column} BETWEEN '{$min}' AND '{$max}'";
+    }
+
+    public function buildCompare(CompareCriteria $criteria)
+    {
+        return $this->buildWithOperator(
+            $criteria->getColumn(),
+            $criteria->getOperator(),
+            $criteria->getValue()
+        );
+    }
+
+    public function buildEqual(EqualCriteria $criteria)
+    {
+        return $this->buildWithOperator(
+            $criteria->getColumn(),
+            "=",
+            $criteria->getValue()
+        );
+    }
+
+    public function buildLike(LikeCriteria $criteria)
+    {
+        return $this->buildWithOperator(
+            $criteria->getColumn(),
+            "LIKE",
+            $criteria->getValue()
+        );
+    }
+
+    public function buildRegexp(RegexpCriteria $criteria)
+    {
+        return $this->buildWithOperator(
+            $criteria->getColumn(),
+            "REGEXP",
+            $criteria->getValue()
+        );
+    }
+
+    protected function buildWithOperator($column, $operator, $value)
+    {
+        $column = $this->buildColumn($column);
+        return "{$column} {$operator} '{$value}'";
+    }
+}
